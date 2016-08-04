2e9cf2d287b7f39f42aed7553fab573b0683a5b2
Rehmat Sakrani <rsakrani@gmail.com>
2016-08-04 09:43:40 -0400

    stick figure file

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(0, 0, 0);
+  fill(255, 255, 255);
+  ellipse(200, 200, 45, 45);
+  stroke(255, 255, 255);
+  line(200, 200, 200, 300);
+  stroke(255, 255, 255);
+  line(200, 300, 220, 330);
+  stroke(255, 255, 255)
+  line(200, 300, 180, 330);
+  stroke(255, 255, 255);
+  line(200, 250, 230, 230);
+  stroke(255, 255, 255);
+  line(200, 250, 170, 230);
+}
